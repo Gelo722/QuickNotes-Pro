81106431ad36ed905dfae6ebfc1deb57787d54c8
Oleg.A <ar.gelo722@gmail.com>
2026-01-30 00:19:06 +0300

    feat: add `trash` table for sql schema

--- server/src/database/schema.sql
+++ server/src/database/schema.sql
@@ -1,6 +1,5 @@
 
 
-
 -- таблица для заметок
 CREATE TABLE IF NOT EXISTS quicknotes (
     id UUID PRIMARY KEY, 
@@ -8,6 +7,13 @@ CREATE TABLE IF NOT EXISTS quicknotes (
     content VARCHAR(255), 
     date DATE, time TIMETZ
 );
+
+CREATE TABLE IF NOT EXISTS quicknotes_trash (
+    id UUID PRIMARY KEY,
+    title VARCAR(255),
+    content VARCHAR(255),
+    date DATE, time TIMETZ
+)
 
 
 
